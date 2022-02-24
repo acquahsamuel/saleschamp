b7894a3c280eae933e7aa7449ee3074b69c8a34b
acquahsamuel <iamsamuelacquah@gmail.com>
2022-02-24 11:53:51 +0000

    [WIP] +> constants,countries,factoryHandler

--- src/utils/factoryHandler.js
+++ src/utils/factoryHandler.js
@@ -1,28 +1,32 @@
-const ErrorResponse = require("./errorResponse");
-const asyncHandler = require("../middleware/async");
-const COUNTRY_LOOKUP = require("./countries");
-const AVAILABLE_STATUS = require("./constants");
+const ErrorResponse = require('./errorResponse');
+const asyncHandler = require('../middleware/async');
+const COUNTRY_LOOKUP = require('./countries');
+const AVAILABLE_STATUS = require('./constants');
 
 /**
-
- * @Request         GET
+ * @description     DELETE single Address
+ * @route           DELETE /api/v1/address/:id
+ * @Request         Delete
  * @Access          Public
  */
-exports.getAll = Model =>
-  asyncHandler(async (req, res, next) => {
+exports.getAll = (Model) =>
+  asyncHandler(async (req, res) => {
     const docs = await Model.find();
     res.status(200).json({
       count: docs.length,
       success: true,
-      data: docs
+      data: docs,
     });
   });
 
 /**
- * @Request         POST
- * @Access          Public - payload
+ * @description     DELETE single Address
+ * @route           DELETE /api/v1/address/:id
+ * @Request         Delete
+ * @Access          Public
  */
-exports.create = Model =>
+exports.create = (Model) =>
+  // eslint-disable-next-line consistent-return
   asyncHandler(async (req, res, next) => {
     const createNew = {
       country: req.body.country,
@@ -30,44 +34,38 @@ exports.create = Model =>
       street: req.body.street,
       postalcode: req.body.postalcode,
       number: req.body.number,
-      numberAddition: req.body.numberAddition
+      numberAddition: req.body.numberAddition,
     };
 
-    /**
-     * Validate country
-     */
     if (!req.body.country) {
-      return next(new ErrorResponse("Country code cannot be null", 400));
+      return next(new ErrorResponse('Country code cannot be null', 400));
     }
 
-    /**
-     * Validate street
-     */
     if (!req.body.street) {
-      return next(new ErrorResponse("Street cannot be null", 400));
+      return next(new ErrorResponse('Street cannot be null', 400));
     }
 
     if (!COUNTRY_LOOKUP.has(req.body.country.toLowerCase())) {
-      return next(new ErrorResponse("Address code is invalid", 400));
+      return next(new ErrorResponse('Address code is invalid', 400));
     }
 
-    /**
-     * Set Header in country
-     */
     const country = COUNTRY_LOOKUP.get(req.body.country.toLowerCase());
-    res.setHeader("Country", country);
+    res.setHeader('Country', country);
     const docs = await Model.create(createNew);
     res.status(201).json({
       success: true,
-      data: docs
+      data: docs,
     });
   });
 
 /**
- * @Request         GET
+ * @description     DELETE single Address
+ * @route           DELETE /api/v1/address/:id
+ * @Request         Delete
  * @Access          Public
  */
-exports.getOne = Model =>
+exports.getOne = (Model) =>
+  // eslint-disable-next-line consistent-return
   asyncHandler(async (req, res, next) => {
     const docs = await Model.findById(req.params.id);
     if (!docs) {
@@ -77,23 +75,23 @@ exports.getOne = Model =>
     }
     res.status(200).json({
       success: true,
-      data: docs
+      data: docs,
     });
   });
 
 /**
- * @Request         PATCH
+ * @description     DELETE single Address
+ * @route           DELETE /api/v1/address/:id
+ * @Request         Delete
  * @Access          Public
  */
-exports.updateOne = Model =>
+exports.updateOne = (Model) =>
+  // eslint-disable-next-line consistent-return
   asyncHandler(async (req, res, next) => {
-    /**
-     * Body data
-     */
-    let data = {
+    const data = {
       status: req.body.status,
       name: req.body.name,
-      email: req.body.email
+      email: req.body.email,
     };
 
     const address = await Model.findById(req.params.id);
@@ -129,12 +127,12 @@ exports.updateOne = Model =>
 
     const docs = await Model.findByIdAndUpdate(req.params.id, data, {
       new: true,
-      runValidators: true
+      runValidators: true,
     });
 
     res.status(200).json({
       success: true,
-      data: docs
+      data: docs,
     });
   });
 
@@ -144,7 +142,8 @@ exports.updateOne = Model =>
  * @Request         Delete
  * @Access          Public
  */
-exports.deleteOne = Model =>
+exports.deleteOne = (Model) =>
+  // eslint-disable-next-line consistent-return
   asyncHandler(async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id);
     if (!doc) {
@@ -154,6 +153,6 @@ exports.deleteOne = Model =>
     }
     res.status(200).json({
       success: true,
-      data: {}
+      data: {},
     });
   });
