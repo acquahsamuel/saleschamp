518910abd3f8250457d0d52f073b20ee71a40b07
acquahsamuel <iamsamuelacquah@gmail.com>
2022-02-24 15:48:56 +0000

    ci: config file

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  });
-  console.log(`MongoDB Connected ---SalesChamp`);
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
