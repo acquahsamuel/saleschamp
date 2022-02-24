76d2875a65b18afcef6f52adae442bf6c6e834a3
acquahsamuel <iamsamuelacquah@gmail.com>
2022-02-24 15:41:02 +0000

    ci: refactoring

--- src/app.js
+++ src/app.js
@@ -1,27 +1,36 @@
-const os = require("os");
-const express = require("express");
-const errorHandler = require("./middleware/error");
-require("dotenv").config();
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const errorHandler = require('./utils/customErrors');
+require('dotenv').config('.env');
 
 /**
  * Connection db
  */
 const app = express();
 app.use(express.json());
+app.use(bodyParser.json());
+app.post(bodyParser.raw({ type: 'application/json' }));
+
+/**
+ * Mongoose endpoint logger
+ */
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 /**
  * Routing
  */
-const address = require("./routes/address");
+const address = require('./routes/addressRoutes');
 
 /**
  * Mount
  */
-app.use("/api/v1/address", address);
-app.use("/", (request, response) => {
-  response.setHeader("Content-Type", "application/json");
+app.use('/api/v1/address', address);
+app.use('/', (req, res) => {
   response.json({
-    message: `SalesChamp deployed - https://saleschampnlapi.herokuapp.com/api/v1/address`
+    message: `Hello -SalesChamp`,
   });
 });
 
