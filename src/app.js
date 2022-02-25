548dcf6c1d5f3a8e6aa1ce93cdb765fcd80d7afb
acquahsamuel <iamsamuelacquah@gmail.com>
2022-02-25 15:17:00 +0000

    app.js

--- src/app.js
+++ src/app.js
@@ -4,33 +4,47 @@ const bodyParser = require('body-parser');
 const errorHandler = require('./utils/customErrors');
 require('dotenv').config('.env');
 
-//Connection db
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.post(bodyParser.raw({ type: 'application/json' }));
 
-//Mongoose endpoint logger
+/**
+ * Logger
+ */
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// Check for content type
-app.use('/api/', function (req, res, next) {
-  var contype = req.headers['content-type'];
+/**
+ * Check for correct connect type
+ */
+app.use('/api/', (req, res, next) => {
+  const contype = req.headers['content-type'];
   if (!contype || contype.indexOf('application/json') !== 0)
-    return res.send(415);
+    return res.sendStatus(415);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, X_BPI_CONTEXT',
+  );
+  res.header('Content-Type', 'application/json');
   next();
 });
 
 // Routing
 const address = require('./routes/addressRoutes');
 
-//Mount content
+// Mount content
 app.use('/api/v1/address', address);
 
 // Error Handler
 app.use(errorHandler);
 
-//Main App Module
+// Main App Module
 module.exports = app;
